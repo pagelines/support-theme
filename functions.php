5c8285cb0956a3454715b336cdda863e2ebf4acd
James Giroux <jgiroux@groundwork.cc>
2014-09-10 11:25:19 -0400

    update subs

--- functions.php
+++ functions.php
@@ -5,9 +5,4 @@
 define( 'DMS_CORE', true );
 require_once( 'dms/functions.php' );
 
-//replace 'PageLines-' prefix for DMS Editor media uploads (from editor/editor.actions.php)
-add_filter( 'pl_up_image_prefix', 'my_pl_upload_prefix');
 
-function my_pl_upload_prefix(){
-	return 'groundwork-'; //example result: 'My Keyword - filename.jpg'
-}
